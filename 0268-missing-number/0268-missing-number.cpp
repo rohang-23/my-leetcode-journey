b070615baa145ebbe2f6bee9d035fc1f32153cf3
rohang-23 <rohan.gude@mitwpu.edu.in>
2025-07-20 03:29:24 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.44%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == i) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
