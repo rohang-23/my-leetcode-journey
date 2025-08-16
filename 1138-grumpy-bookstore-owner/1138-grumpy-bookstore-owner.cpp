c8302ac948c2b212094714fd9f1e8b74be24eec8
rohang-23 <rohan.gude@mitwpu.edu.in>
2025-08-16 17:43:27 +0530

    Time: 0 ms (100%), Space: 35.9 MB (7.79%) - LeetHub

---
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int k = minutes;
+        vector<int>&arr = customers;
+        int n = arr.size();
+        int prevLoss = 0;
+        for(int i=0;i<n;i++){
+            if(grumpy[i]==1) prevLoss += arr[i];
+        }
+        int currLoss = prevLoss;
+        int i=1;
+        int j=k;
+        int maxLoss = prevLoss;
+        int maxIdx = 0;
+        while(j<n){
+            currLoss = prevLoss;
+            if(grumpy[j]==1) currLoss += arr[j];
+            if(grumpy[i-1]==1) currLoss -= arr[i-1];
+            if(maxLoss<currLoss){
+                maxLoss = currLoss;
+                maxIdx = i;
+            }
+            prevLoss = currLoss;
+            i++;
+            j++;
+        }
+        for(int i=maxIdx;i<maxIdx+k;i++){
+            grumpy[i] = 0;
+        }
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            if(grumpy[i]==0) sum += arr[i];
+        }
+        return sum;
+    }
+};
