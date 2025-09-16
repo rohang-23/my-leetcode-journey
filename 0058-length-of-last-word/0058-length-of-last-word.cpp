8d17594747fa7cdc0ca6e7fbb65c7356eecb2a77
rohang-23 <rohan.gude@mitwpu.edu.in>
2025-09-16 15:48:58 +0530

    Time: 0 ms (100%), Space: 9 MB (32.23%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        int n =s.length();
+        bool flag=false;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]!=' '){
+             flag =true;
+                count++;
+            }
+            else if(s[i]==' ' && flag==true){
+                break;
+            }
+        }
+        return count;
+    }
+};
