1f6e74f7896c21c83b3c90eadeec6a72d4164d20
rohang-23 <rohan.gude@mitwpu.edu.in>
2025-11-28 14:58:17 +0530

    Time: 3 ms (52.39%), Space: 25 MB (97.65%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         vector<int> arr;
-        int i=0;
+        int i = 0;
         int missingno;
         int duplicatedno;
-        for(int i=0;i<n-1;i++){
-                if(nums[i+1]==nums[i]){
-                    duplicatedno=nums[i];
-                    break;
-                }
-        }
-            int actualsum =(n *(n+1))/2; 
-            int currsum=0;
-            for(int i=0;i<n;i++){
-                currsum += nums[i];
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i + 1] == nums[i]) {
+                duplicatedno = nums[i];
+                break;
             }
-            missingno =  actualsum - (currsum - duplicatedno);
-        
-        return {duplicatedno,missingno};
+        }
+        int actualsum = (n * (n + 1)) / 2;
+        int currsum = 0;
+        for (int i = 0; i < n; i++) {
+            currsum += nums[i];
+        }
+        missingno = actualsum - (currsum - duplicatedno);
+        return {duplicatedno, missingno};
     }
 };
