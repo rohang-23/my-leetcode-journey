37df2e5ed762d05ffd003d134fae3a1aef119017
rohang-23 <rohan.gude@mitwpu.edu.in>
2025-12-30 21:05:11 +0530

    Time: 10 ms (46.67%), Space: 11.4 MB (21.51%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -5,17 +5,21 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
         ListNode* temp = head;
-
+        int flag =false;
         while(fast!=NULL && fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
             if(slow==fast){
-                while(slow!=temp){
-                    slow=slow->next;
-                    temp=temp->next;
-                }
-                return slow;
+               flag=true;
+               break;
             } 
+        }
+        if(flag==true){
+            while(slow!=temp){
+                slow=slow->next;
+                temp=temp->next;
+            }
+            return slow;
         }
         return NULL;
     }
