60a5903fd9d4970b9dc12c8a65319d3417b05aad
rohang-23 <rohan.gude@mitwpu.edu.in>
2026-01-03 15:45:19 +0530

    Time: 0 ms (100%), Space: 15 MB (43.14%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* lo = new ListNode(100);
+        ListNode* hi = new ListNode(130);
+        ListNode* tempLo = lo;
+        ListNode* tempHi = hi;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            if(temp->val < x){
+                tempLo->next = temp;
+                tempLo = tempLo->next;
+                temp = temp->next;
+            }
+            else{
+                tempHi->next = temp;
+                tempHi = tempHi->next;
+                temp = temp->next;
+            }
+        }
+        tempLo->next = hi->next;
+        tempHi->next = NULL;
+        return lo->next;
+        
+    }
+};
