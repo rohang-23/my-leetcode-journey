22d449a39cd5683d86b1601172456e525becd47e
rohang-23 <rohan.gude@mitwpu.edu.in>
2026-03-12 23:14:19 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.75%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -4,9 +4,7 @@ public:
     bool isPowerOfTwo(int n) {
        if(n==1) return true;
        if(n<=0) return false;
-        // if(n%2==0 && (n/2)%2==0) return true;
-        // else return false;
-        for(;n>1;){
+        while(n>1){
             if(n%2!=0){
                 return false;
             }
