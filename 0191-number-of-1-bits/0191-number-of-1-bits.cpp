e3267ebe20ab5a32be335682a2779b1abcac8255
rohang-23 <rohan.gude@mitwpu.edu.in>
2026-03-14 03:26:39 +0530

    Time: 0 ms (100%), Space: 8.3 MB (47.83%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-           int count = 0;
-        //    while(n != 0){
-        //     n = n & (n-1);
-        //     count++;
-        //    }
-        while(n>0){
-            count += n & 1;
-            n = n >> 1;
-            
-        }
+      int count = 0;
+      while(n !=0){
+        n = n & (n-1);
+        count++;
+      }
            return count;
     }
 };
