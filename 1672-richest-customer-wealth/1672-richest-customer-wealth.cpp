68f9dee89571168bff109a9fd212604f89d8a01e
Rohan Gude <rohan.gude@mitwpu.edu.in>
2026-09-04 20:55:41 +0530

    Time: 0 ms (100%), Space: 11.4 MB (55.12%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int n=accounts.size();
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            int maximum = 0;
+            int m=accounts[i].size();
+            for(int j=0;j<m;j++)
+            {
+                maximum += accounts[i][j];
+            }
+            ans = max(ans,maximum);
+        }
+        return ans;
+    }
+};
